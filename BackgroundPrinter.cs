cd172b2bffcbc371342b60f5766b6f14313ceb87
Diego Aquino <diegol.aquino@gmail.com>
2020-11-25 23:00:56 -0300

    Add project files.

---
+++ BackgroundPrinter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostedService
+{
+    public class BackgroundPrinter : IHostedService, IDisposable
+    {
+        private readonly ILogger<BackgroundPrinter> logger;
+        private int _number = 0;
+        private Timer _timer;
+
+        public BackgroundPrinter(ILogger<BackgroundPrinter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(o =>
+            {
+                Interlocked.Increment(ref _number);
+                logger.LogInformation($"Eu aqui de novo seus putos -> {_number}");
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Vou parar por aqui seus putos");
+
+            _timer = new Timer(o =>
+            {
+                Interlocked.Increment(ref _number);
+            }, null, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+    }
+}
